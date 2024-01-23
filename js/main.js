57318e3776299378840b43a50a2e92035ca62421
mtteeng <144452459+mtteeng@users.noreply.github.com>
2024-01-23 15:19:55 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+// let name = prompt('Введите ваше имя');
+// alert(`Привет, ${name}!`)
+
+// let a = 1, b = 1
+// let c = ++a;
+// let d = b++;
+// alert(a); //a = 2
+// alert(b); //b = 2
+// alert(c); //с = 2
+// alert(d); //d = 1
+
+let a = 2;
+let x = 1 + (a*=2);
+alert(a);
+alert(x);
